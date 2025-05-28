50ffe0d6ae7ed70322c59e14658876c0b289ac41
ALEX RICHARD DA SILVA <1090482211002@fatec-srv.local>
2025-05-28 09:27:16 -0300

    TRABALHO 2 finalizando

--- back-end/src/controllers/cars.js
+++ back-end/src/controllers/cars.js
@@ -1,114 +1,95 @@
+// Importando o Prisma Client
 import prisma from '../database/client.js'
 
 const controller = {}   // Objeto vazio
 
+// Criando um novo carro
 controller.create = async function (req, res) {
   try {
-    // Dentro do parâmetro req (requisição), haverá
-    // um objeto chamado "body" que contém as informações
-    // que queremos armazenar do BD. Então, invocamos o
-    // Prisma para fazer a interface com o BD, repassando
-    // o req.body
     await prisma.car.create({ data: req.body })
 
-    // Se der tudo certo, enviamos como resposta o 
-    // código HTTP apropriado, no caso
     // HTTP 201: Created
     res.status(201).end()
   }
   catch(error) {
-    // Se algo de errado acontecer, cairemos aqui
-    // Nesse caso, vamos exibir o erro no console e enviar
-    // o código HTTP correspondente a erro do servidor
+    console.log(error)
+
     // HTTP 500: Internal Server Error
-    console.error(error)
     res.status(500).end()
   }
 }
 
 controller.retrieveAll = async function (req, res) {
   try {
-    // Recupera todos os registros de carros do banco de dados,
-    // ordenados pelo campo "model"
-    const result = await prisma.car.findMany({
-      orderBy: [ { model: 'asc' } ]
-    })
+    const result = await prisma.car.findMany()
 
     // HTTP 200: OK (implícito)
     res.send(result)
+
   }
   catch(error) {
-    // Se algo de errado acontecer, cairemos aqui
-    // Nesse caso, vamos exibir o erro no console e enviar
-    // o código HTTP correspondente a erro do servidor
+    console.log(error)
+
     // HTTP 500: Internal Server Error
-    console.error(error)
     res.status(500).end()
   }
 }
 
-controller.retrieveOne = async function (req, res) {
+controller.retrieveOne = async function(req, res) {
   try {
-    // Busca no banco de dados apenas o carro indicado
-    // pelo parâmetro "id"
     const result = await prisma.car.findUnique({
       where: { id: Number(req.params.id) }
     })
 
-    // Encontrou ~> HTTP 200: OK (implícito)
+    // Encontrou: retorna HTTP 200: OK
     if(result) res.send(result)
-
-    // Não encontrou ~> HTTP 404: Not found
+    // Não encontrou: retorna HTTP 404: Not Found
     else res.status(404).end()
   }
   catch(error) {
-    // Se algo de errado acontecer, cairemos aqui
-    // Nesse caso, vamos exibir o erro no console e enviar
-    // o código HTTP correspondente a erro do servidor
+    console.log(error)
+
     // HTTP 500: Internal Server Error
-    console.error(error)
     res.status(500).end()
   }
 }
 
 controller.update = async function(req, res) {
   try {
-    // Busca o registro no banco de dados pelo seu id
-    // e atualiza as informações com o conteúdo de req.body
-    await prisma.car.update({
+    const result = await prisma.car.update({
       where: { id: Number(req.params.id) },
       data: req.body
     })
 
-    // Encontrou e atualizou ~> HTTP 204: No content
-    res.status(204).end()
+    // Encontrou e atualizou: retorna HTTP 204: No Content
+    if(result) res.status(204).end()
+    // Não encontrou (e não atualizou): retorna HTTP 404: Not Found
+    else res.status(404).end()
   }
   catch(error) {
-    console.error(error)
-    
-    // Não encontrou e não atualizou ~> HTTP 404: Not found
-    if(error?.code === 'P2025') res.status(404).end()
-    // Outros tipos de erro ~> HTTP 500: Internal server error
-    else res.status(500).end()
+    console.log(error)
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
   }
 }
 
-controller.delete = async function(req, res) {
+controller.delete = async function (req, res) {
   try {
-    await prisma.car.delete({
+    const result = await prisma.car.delete({
       where: { id: Number(req.params.id) }
     })
 
-    // Encontrou e excluiu ~> HTTP 204: No content
-    res.status(204).end()
+    // Encontrou e excluiu ~> HTTP 204: No Content
+    if(result) res.status(204).end()
+    // Não encontrou (e não excluiu) ~> HTTP 404: Not Found
+    else res.status(404).end()
   }
   catch(error) {
-    console.error(error)
-    
-    // Não encontrou e não excluiu ~> HTTP 404: Not found
-    if(error?.code === 'P2025') res.status(404).end()
-    // Outros tipos de erro ~> HTTP 500: Internal server error
-    else res.status(500).end()
+    console.log(error)
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
   }
 }
 
--- front-end/src/pages/cars/CarsForm.jsx
+++ front-end/src/pages/cars/CarsForm.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
-import { ptBR } from 'date-fns/locale/pt-BR'
-import { parseISO } from 'date-fns'
-import MenuItem from '@mui/material/MenuItem'
-import Button from '@mui/material/Button'
-import InputMask from 'react-input-mask'
-import { feedbackWait, feedbackNotify, feedbackConfirm } from '../../ui/Feedback'
-import { useNavigate, useParams } from 'react-router-dom'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Checkbox from '@mui/material/Checkbox'
+import { Checkbox, FormControlLabel } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
+import React from 'react';
+import InputMask from 'react-input-mask';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ZodError } from 'zod';
+import myfetch from '../../lib/myfetch';
+import Car from '../../models/Car';
+import useConfirmDialog from '../../ui/useConfirmDialog';
+import useNotification from '../../ui/useNotification';
+import useWaiting from '../../ui/useWaiting';
 
-import fetchAuth from '../../lib/fetchAuth'
+export default function CarForm() {
+  /*
+    Por padrão, todos os campos do nosso formulário terão como
+    valor inicial uma string vazia. A exceção é o campo birth_date
+    que, devido ao funcionamento do componente DatePicker, deve
+    iniciar valendo null.
+  */
 
-export default function CarsForm() {
+  const formDefaults = {
+    brand: '',
+    model: '',
+    color: '',
+    year_manufacture: '',
+    imported: false,
+    plates: '',
+    selling_date: null,
+    selling_price: '',
+  };
 
-  // Lista de cores dos carros em ordem alfabética disponíveis para o usúario escolher
+  const [state, setState] = React.useState({
+    car: { ...formDefaults },
+    formModified: false,
+    inputErrors: {},
+  });
+  const { car, formModified, inputErrors } = state;
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const { askForConfirmation, ConfirmDialog } = useConfirmDialog();
+  const { notify, Notification } = useNotification();
+  const { showWaiting, Waiting } = useWaiting();
+
   const colors = [
     { value: 'AMARELO', label: 'AMARELO' },
     { value: 'AZUL', label: 'AZUL' },
@@ -33,294 +65,276 @@ export default function CarsForm() {
     { value: 'ROXO', label: 'ROXO' },
     { value: 'VERDE', label: 'VERDE' },
     { value: 'VERMELHO', label: 'VERMELHO' },
-  ]
+  ];
 
-  // Máscara para o formato da placa do carro
-  const platesMaskFormatChars = {
-    '9': '[0-9]',    // somente dígitos
-    '$': '[0-9A-J]',  // dígito de 0 a 9 ou uma letra de A a J.
-    'A': '[A-Z]',
+  const plateMaskFormatChars = {
+    9: '[0-9]', // somente dígitos
+    $: '[0-9A-J]', // dígito de 0 a 9 ou uma letra de A a J.
+    A: '[A-Z]', //  letra maiúscula de A a Z.
+  };
+
+  const currentYear = new Date().getFullYear();
+  const minYear = 1960;
+  const years = [];
+  for (let year = currentYear; year >= minYear; year--) {
+    years.push(year);
   }
 
-  // Cria um vetor com os anos disponíveis, do ano atual até 1951
-  const years = [] 
-  // Date() pega a data atual. ex: 8 de novembro de 2024. O getFullYear() pega só o ano, ex: 2024
-  for (let year = new Date().getFullYear(); year >= 1951; year--) {   
-    years.push(year) // add no vetor years
-  }
+  const [imported, setImported] = React.useState(false);
+  // car.imported = imported;
+  const handleImportedChange = (event) => {
+    setImported(event.target.checked);
+  };
 
-  /* Defini os valores padrão do formulário que são uma string vazia. selling_price e selling_date 
-  são null, porque são campos não obrigatórios. e imported é false porque é um bolleano */
-  const formDefaults = {
-    brand: '',
-    model: '',
-    color: '',
-    year_manufacture: '',
-    imported: false,
-    plates: '',
-    selling_price: '',
-    selling_date: null
-  }
-
-  const navigate = useNavigate()
-  const params = useParams()
-
-  const [state, setState] = React.useState({
-    car: { ...formDefaults },
-    formModified: false
-  })
-  const {
-    car,
-    formModified
-  } = state
-
-  /*Se estivermos editando um carro, precisamos carregar seus dados assim que o componente for carregado */
-  React.useEffect(() => {
-    /*Sabemos que estamos editando (e não cadastrando um novo) carro quando a rota ativa contiver um parâmetro id */
-    if (params.id) loadData()
-  }, [])
-
-  // Função para carregar os dados de um carro existente da API
-  async function loadData() {
-    feedbackWait(true)
-    try {
-      const response = await fetchAuth.get('//' + params.id)
-      const result = await response.json()
-      
-      /*Converte o formato da data armazenado no banco de dados para o formato reconhecido pelo componente DatePicker */
-      if(result.selling_date) result.selling_date = parseISO(result.selling_date)
-      setState({ ...state, car: result, formModified: false })
-    }
-    catch(error) {
-      console.log(error)
-      feedbackNotify('ERRO: ' + error.message, 'error')
-    }
-    finally {
-      feedbackWait(false)
-    }
-  }
-
-  /* Preenche o campo do objeto car conforme o campo correspondente do formulário for modificado */
   function handleFieldChange(event) {
-    // Tira uma cópia da variável de estado car
-    const carsCopy = { ...car }
-    // Altera em carsCopy apenas o campo da vez
-    carsCopy[event.target.name] = event.target.value
-    // Atualiza a variável de estado, substituindo o objeto car por sua cópia atualizada
-    setState({ ...state, car: carsCopy, formModified: true })
+    const carCopy = { ...car };
+    carCopy[event.target.name] = event.target.value;
+    setState({ ...state, car: carCopy, formModified: true });
   }
 
-  // Função para salvar os dados do formulário
   async function handleFormSubmit(event) {
-    event.preventDefault()      // Impede o recarregamento da página
-    feedbackWait(true)
+    event.preventDefault(); // Evita que a página seja recarregada
+    showWaiting(true); // Exibe a tela de espera
     try {
-      /* Infoca o fetch para enviar os dados ao back-end.
-      Se houver parâmetro na rota, significa que estamos alterando
-      um registro existente e, portanto, o verbo precisa ser PUT */
-      if(params.id) {
-        await fetchAuth.put('/cars/' + params.id, car)
-      }
-      // Senão, envia com o método POST para criar um novo registro
-      else {
-        await fetchAuth.post('/cars', car)
-      }
+      // Invoca a validação dos dados da biblioteca Zod
+      // por meio do model Car === '' ? '' : parseFloat(value);
 
-      // Exibe uma mensagem de sucesso e vai para a página de listagem dos carros
-      feedbackNotify('Item salvo com sucesso.', 'success', 4000, () => {
-        // Retorna para a página de listagem
-        navigate('..', { relative: 'path', replace: true })
-      })
+      Car.parse(car);
+      console.log(car);
 
-    }
-    catch(error) {
-      console.log(error)
-      feedbackNotify('ERRO: ' + error.message, 'error')
-    }
-    finally {
-      feedbackWait(false)
+      // Se houver parâmetro na rota, significa que estamos modificando
+      // um cliente já existente. A requisição será enviada ao back-end
+      // usando o método PUT
+      if (params.id) await myfetch.put(`/cars/${params.id}`, car);
+      // Caso contrário, estamos criando um novo cliente, e enviaremos
+      // a requisição com o método POST
+      else await myfetch.post('/cars', car);
+
+      // Deu certo, vamos exbir a mensagem de feedback que, quando for
+      // fechada, vai nos mandar de volta para a listagem de clientes
+      notify('Item salvo com sucesso.', 'success', 4000, () => {
+        navigate('..', { relative: 'path', replace: true });
+      });
+    } catch (error) {
+      // console.error(error);
+      if (error instanceof ZodError) {
+        // Formamos um objeto contendo os erros do Zod e
+        // o colocamos na variável de estado inputErrors
+        const messages = {};
+        for (let i of error.issues) messages[i.path[0]] = i.message;
+        setState({ ...state, inputErrors: messages });
+        notify('Há campos com valores inválidos no formulário', 'error');
+      } else notify(error.message, 'error');
+    } finally {
+      // Desliga a tela de espera, seja em caso de sucesso, seja em caso de erro
+      showWaiting(false);
     }
   }
 
-  // Função para voltar para a página anterior
-  async function handleBackButtonClick() {
-    if(
-      formModified && 
-      ! await feedbackConfirm('Há informações não salvas. Deseja realmente voltar?')
-    ) return // Sai da função sem fazer nada
+  /*
+    useEffect() que é executado apenas uma vez, no carregamento do componente.
+    Verifica se a rota tem parâmetro. Caso tenha, significa que estamos vindo
+    do componente de listagem por meio do botão de editar, e precisamos chamar
+    a função loadData() para buscar no back-end os dados do cliente a ser editado
+  */
+  React.useEffect(() => {
+    if (params.id) loadData();
+  }, []);
 
-    // Aqui o usuário respondeu que quer voltar e perder os dados
-    navigate('..', { relative: 'path', 'replace': true })
+  async function loadData() {
+    showWaiting(true);
+    try {
+      const result = await myfetch.get(`/cars/${params.id}`);
+
+      // Converte o formato de data armazenado no banco de dados
+      // para o formato reconhecido pelo componente DatePicker
+      result.birth_date = parseISO(result.birth_date);
+
+      setState({ ...state, customer: result });
+    } catch (error) {
+      console.error(error);
+      notify(error.message, 'error');
+    } finally {
+      showWaiting(false);
+    }
+  }
+
+  async function handleBackButtonClick() {
+    if (
+      formModified &&
+      !(await askForConfirmation(
+        'Há informações não salvas. Deseja realmente sair?'
+      ))
+    )
+      return; // Sai da função sem fazer nada
+
+    // Navega de volta para a página de listagem
+    navigate('..', { relative: 'path', replace: true });
   }
 
   return (
     <>
-      { /* gutterBottom coloca um espaçamento extra abaixo do componente */ }
-      <Typography variant="h1" gutterBottom>
-        {params.id ? `Editar veículo #${params.id}` : 'Cadastrar novo veículo'}
+      <ConfirmDialog />
+      <Notification />
+      <Waiting />
+
+      <Typography variant='h1' gutterBottom>
+        {params.id ? `Editar carro #${params.id}` : 'Cadastrar novo carro'}
       </Typography>
 
-      <Box className="form-fields">
+      <Box className='form-fields'>
         <form onSubmit={handleFormSubmit}>
-
-          {/* autoFocus = foco do teclado no primeiro campo */}
           <TextField
-            variant="outlined" 
-            name="brand"
-            label="Marca do carro"
-            fullWidth
+            name='brand'
+            label='Marca do carro'
+            variant='filled'
             required
-            autoFocus
+            fullWidth
             value={car.brand}
             onChange={handleFieldChange}
+            helperText={inputErrors?.brand}
+            error={inputErrors?.brand}
           />
           <TextField
-            variant="outlined" 
-            name="model"
-            label="Modelo do carro"
-            fullWidth
+            name='model'
+            label='Modelo do carro'
+            variant='filled'
             required
+            fullWidth
             value={car.model}
             onChange={handleFieldChange}
+            helperText={inputErrors?.model}
+            error={inputErrors?.model}
           />
+
           <TextField
-            select
-            variant="outlined" 
-            name="color"
-            label="Cor"
-            fullWidth
+            name='color'
+            label='Color'
+            variant='filled'
             required
+            fullWidth
             value={car.color}
             onChange={handleFieldChange}
-          > 
-          {/* Lista de cores para selecionar */}
-          {colors.map(s => 
-                <MenuItem key={s.value} value={s.value}>
-                  {s.label}
-                </MenuItem>
-              )}
-          </TextField>
-          <TextField
             select
-            variant="outlined" 
-            name="year_manufacture"
-            label="Ano de fabricação"
-            fullWidth
+            helperText={inputErrors?.state}
+            error={inputErrors?.state}
+          >
+            {colors.map((s) => (
+              <MenuItem key={s.value} value={s.value}>
+                {s.label}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            name='year_manufacture'
+            label='Ano de fabricação'
+            variant='filled'
             required
+            fullWidth
+            select
             value={car.year_manufacture}
             onChange={handleFieldChange}
+            helperText={inputErrors?.year_manufacture}
+            error={inputErrors?.year_manufacture}
           >
-            {/* Lista de anos para selecionar */}
-            {years.map(y => 
-                <MenuItem key={y} value={y}>
-                  {y}
-                </MenuItem>
-              )}
+            {years.map((year) => (
+              <MenuItem key={year} value={year}>
+                {year}
+              </MenuItem>
+            ))}
           </TextField>
 
-          {/* Checkbox para marcar se o carro é importado */}
-          <div className="MuiFormControl-root">
-            <FormControlLabel
-              control={
-                <Checkbox
-                  name='imported'
-                  checked={car.imported}
-                  onChange={(event)=> 
-                    setState({ ...state, car: { ...car, imported: event.target.checked}, formModified: true})
-                  }
-                />
-              }
-              label='É importado?'
-            />
-          </div>    
-          {/* Campo para placa do carro com a máscara*/}
-          <InputMask
-            mask='AAA-9$99'
-            value={car.plates}
-            onChange={handleFieldChange}
-            formatChars={platesMaskFormatChars}
-          >
-            { () => 
-                <TextField
-                  variant="outlined" 
-                  name="plates"
-                  label="Placa" 
-                  fullWidth
-                  required
-                />
+          <FormControlLabel
+            control={
+              <Checkbox
+                name='imported'
+                variant='filled'
+                value={(car.imported = imported)}
+                checked={imported}
+                onChange={handleImportedChange}
+                color='primary'
+              />
             }
-          </InputMask>
-          <TextField
-            variant="outlined" 
-            name="selling_price"
-            label="Preço de venda"
-            fullWidth
-            required
-            type='number'
-            value={car.selling_price}
-            onChange={handleFieldChange}
+            label='Importado'
           />
 
-          {/*
-            O evento onChange do componente DatePicker não passa
-            o parâmetro event, como no TextField, e sim a própria
-            data que foi modificada. Por isso, ao chamar a função
-            handleFieldChange no DatePicker, precisamos criar um
-            parâmetro event "fake" com as informações necessárias
-          */}
-          <LocalizationProvider 
+          <InputMask
+            mask='AAA-9$99'
+            formatChars={plateMaskFormatChars}
+            maskChar=' '
+            value={car.plates}
+            onChange={handleFieldChange}
+          >
+            {() => (
+              <TextField
+                name='plates'
+                label='Placa'
+                variant='filled'
+                required
+                fullWidth
+                helperText={inputErrors?.phone}
+                error={inputErrors?.phone}
+              />
+            )}
+          </InputMask>
+
+          <LocalizationProvider
             dateAdapter={AdapterDateFns}
             adapterLocale={ptBR}
           >
             <DatePicker
-              label="Data de venda"
-              value={car.selling_date || null}
+              label='Data de venda'
+              value={car.selling_date}
+              onChange={(value) =>
+                handleFieldChange({
+                  target: { name: 'selling_date', value },
+                })
+              }
               slotProps={{
                 textField: {
-                  variant: 'outlined',
-                  fullWidth: true
-                }
-              }}
-              onChange={ date => {
-                const event = { target: { name: 'selling_date', value: date } }
-                handleFieldChange(event)
+                  variant: 'filled',
+                  fullWidth: true,
+                  helperText: inputErrors?.selling_date,
+                  error: inputErrors?.selling_date,
+                },
               }}
             />
           </LocalizationProvider>
 
-          <Box sx={{ 
-            display: 'flex',
-            justifyContent: 'space-around',
-            width: '100%'
-          }}>
-            <Button
-              variant="contained"
-              color="secondary"
-              type="submit"
-            >
+          <TextField
+            name='selling_price'
+            label='Preço de venda'
+            variant='filled'
+            type='number'
+            required
+            fullWidth
+            value={car.selling_price}
+            onChange={handleFieldChange}
+            helperText={inputErrors?.selling_price}
+            error={inputErrors?.selling_price}
+          />
+
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              width: '100%',
+            }}
+          >
+            <Button variant='contained' color='secondary' type='submit'>
               Salvar
             </Button>
-
-            <Button
-              variant="outlined"
-              onClick={handleBackButtonClick}
-            >
+            <Button variant='outlined' onClick={handleBackButtonClick}>
               Voltar
             </Button>
           </Box>
 
-          <Box sx={{
-            fontFamily: 'monospace',
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%'
-          }}>
-            {JSON.stringify(car, null, 2)}
-          </Box>
-
+          {/* <Box sx={{ fontFamily: 'monospace', display: 'flex', width: '100%' }}>
+            {JSON.stringify(inputErrors)}
+          </Box> */}
         </form>
       </Box>
-      
     </>
-  )
+  );
 }
